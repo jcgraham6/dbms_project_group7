0c02dfb3c90cf53bc767a2be65594088d04ad482
James <72426402+jcgraham6@users.noreply.github.com>
2025-04-21 11:40:05 -0400

    Update grocerystoreapp.py
    Adding in more functionalities

--- grocerystoreapp.py
+++ grocerystoreapp.py
@@ -7,10 +7,7 @@ def connect_to_db():
 
     try:
         
-       # cx_Oracle.init_oracle_client()
-
         dsn = oracledb.makedsn('oracle.wpi.edu', 1521, sid = 'ORCL')
-
         db = oracledb.connect(user = 'jgraham',
                                password = 'JGRAHAM',
                                dsn = dsn)
@@ -82,6 +79,7 @@ app = Flask(__name__)
 # Secret key for session
 app.secret_key = os.urandom(24)
 
+
 @app.route('/', methods=['GET', 'POST']) # Route is the place to go (/ represents the base url or the homepage)
 def login():
     if request.method == 'POST':
@@ -100,10 +98,12 @@ def login():
     
     return render_template('login.html')
 
+
 @app.route('/home')
 def home():
 
     return render_template('DailyPick.html')
+
 
 @app.route('/profile', methods = ['GET','POST'])
 def profile():
@@ -112,41 +112,50 @@ def profile():
         # Get the user email from session
         username = session.get('user')
 
+        # If user doesn't exist throw an error
         if not username:
             return "Unauthorized", 401  # Or redirect to login
 
+        # SQL for grabbing user info
         account_sql = '''
             SELECT username, phone, paypref
             FROM ACCOUNT
             WHERE username = :1
         '''
-
+        
+        # Store information in DF
         df = fetch_data(account_sql, [(username)])
-# Account has no ties to the preferences table
 
+        # Collect user preferences
         if request.method == 'POST':
 
             diet = request.form.get('diet')
             brand = request.form.get('brand')
             budget = request.form.get('budget')
 
+            # Check if user already exists in preferences table
             pref_sql = '''
             SELECT * FROM PREFERENCES WHERE USERNAME = :1
             '''
 
+            # Store results in df
             pref_df = fetch_data(pref_sql, [(username)])
-        
+
+            # If empty use INSERT statement
             if pref_df.empty == True:
                 insert_sql = '''
                 INSERT INTO PREFERENCES (username, max_budget, dietaryrestric, prefbrands)
                 VALUES (:1, :2, :3, :4)
                 '''
 
+                # Execute
                 insert_data(insert_sql, [(username, budget, diet, brand)])
 
+                # Flash success message
                 flash("Preferences saved successfully!", "success")
 
-            
+
+            # Else use UPDATE statement
             else:
                 update_sql = '''
                 UPDATE PREFERENCES SET
@@ -156,18 +165,19 @@ def profile():
                 WHERE USERNAME = :4
                 '''
 
+                # Execute
                 update_data(update_sql, [(budget, diet, brand, username)])
 
+                # Flash message
                 flash("Preferences saved successfully!", "success")
 
-
-
+        # Return template
         return render_template('Account.html',
                                username=df['USERNAME'].iloc[0],
                                phone=df['PHONE'].iloc[0],
                                credit_card= df['PAYPREF'].iloc[0])
     
-
+    # Error handling
     except oracledb.DatabaseError as e:
         print(f"Database Error: {e}")
         return "Internal Server Error", 500
@@ -175,6 +185,8 @@ def profile():
 
 @app.route('/createaccount', methods=['GET', 'POST'])
 def createaccount():
+
+    # Collect new account details
     if request.method == 'POST':
         try:
             email = request.form['email']
@@ -184,19 +196,24 @@ def createaccount():
             saveditem = request.form['saveditem']
             paypref = request.form['paypref']
 
+            # INSERT statement
             insert_sql = '''
             INSERT INTO ACCOUNT (createDate, phone, password, email, username, saveditem, paypref)
             VALUES (CURRENT_DATE, :1, :2, :3, :4, :5, :6)
             '''
 
+            # Execute
             insert_data(insert_sql, [(phone, password, email, username, saveditem, paypref)]) 
+
+            # Return to login page
             return redirect(url_for('login'))  # Redirect to login
 
+        # Error if incomplete form
         except KeyError as e:
             print(f"KeyError: Missing fields for {str(e)}")
             return "Missing data", 400
 
-    # Render the CreateAccount form
+    # Render the template
     return render_template('CreateAccount.html')
 
 
@@ -207,35 +224,44 @@ def shop():
     conn = connect_to_db()
     cursor = conn.cursor()
 
+    # Pull commodity data
     data = cursor.execute("SELECT NAME, PRICE, QUANTITY FROM COMMODITY_STORE")
 
+    # Get query from search bar
     search_query = request.args.get('search', '') 
 
+    # Initialize empty list
     filtered_data = []
 
+    # Filter commodity table based on search query
     if search_query:
         filtered_data = [row for row in data if search_query.lower().strip() in row[0].lower()]
+
+    # If not search then just return full table
     else:
         filtered_data = data
 
-    return render_template('Search.html', data=filtered_data)  # Here, you're passing `filtered_data`
-
+    # Render template and pass data table
+    return render_template('Search.html', data=filtered_data)
 
 
 @app.route('/go_to_shop')
 def go_to_shop():
-    # This route will redirect the user to the shop page
+
+    # Render template
     return redirect(url_for('shop'))
 
 
 @app.route('/add_to_cart', methods=['POST'])
 def add_to_cart():
+
+    # Collect commodity details
     try:
         product_name = request.form['product_name']
         product_price = float(request.form['price'])
         product_quantity = int(request.form['quantity'])
 
-        # Retrieve the cart from the session (if it exists)
+        # Retrieve the cart from the session
         cart = session.get('cart', [])
         
         # Check if the product is already in the cart
@@ -258,9 +284,10 @@ def add_to_cart():
         session['cart'] = cart
         session.modified = True
 
-        # Refresh page
+        # Render template with saved cart
         return redirect(url_for('cart'))
 
+    # Error handling
     except KeyError as e:
         print(f"KeyError: Missing form data for {str(e)}")
         return "Missing data", 400
@@ -302,26 +329,30 @@ def cart():
     return render_template('Cart.html', cart=cart)
 
 
-
-@app.route('/go_to_signin')
-def go_to_signin():
-    # This route will redirect the user to the signin page
-    return redirect(url_for('signin'))
-
 @app.route('/contactus', methods=['GET', 'POST'])
 def contactus():
+
+    # Collect submission
     if request.method=='POST':
         name=request.form['name']
         email=request.form['email']
         message=request.form['message']
-        conn=connect_to_db()
-        cur=conn.cursor()
-        cur.execute("INSERT INTO contactus (name, email, message) VALUES(%s, %s, %s)", (name , email, message))
-        conn.commit()
-        cur.close()
+
+        # INSERT statement
+        contact_sql = '''
+        INSERT INTO contactus (name, email, message) 
+        VALUES(:1, :2, :3)
+        '''
+        # Execute 
+        insert_data(contact_sql, [(name , email, message)])
+        
+        # Return template
         return render_template('thankyou.html')
+    
+    # Render template
     return render_template('contactus.html')
 
+    
 @app.route('/product/<int:commodityID>')
 def product_detail(commodityID):
     custID=session.get('custID') #Fetch from session
@@ -337,20 +368,28 @@ def product_detail(commodityID):
     conn.close()
     return render_template('product_detail.html', product=product, reviews=reviews)
 
+
 @app.route('/submit_review', methods=['POST'])
 def submit_review():
+
+    # Send back to login page
     if 'custID' not in session:
-        return redirect('/SignIn')
+        return redirect('/')
+    
     commodityID=request.form['commodityID']
     custID=request.form['custID']
     rating=request.form['rating']
     comment=request.form['comment']
-    conn=connect_to_db()
-    cursor=conn.cursor()
-    cursor.execute("""INSERT INTO Review(reviewID, commodityID, custID, rating, comment, reviewDate) VALUES (review_seq.NEXTVAL, :commodityID, :custID, :rating, :comment, SYSDATE)""", [commodityID , custID, rating, comment])
-    conn.commit()
-    cursor.close()
-    conn.close()
+    
+    submit_sql = '''
+    INSERT INTO Review(reviewID, commodityID, custID, rating, comment, reviewDate) 
+    VALUES (review_seq.NEXTVAL, :commodityID, :custID, :rating, :comment, SYSDATE)
+    '''
+     
+    insert_data(submit_sql, [(commodityID , custID, rating, comment)])
+
     return render_template(f'/product/{commodityID}')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
